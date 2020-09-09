6846f813ec017da649a3403021fb2792315fdc56
sergey <mketsvil2@gmail.com>
2020-09-10 02:45:06 +0600

    Работа с атрибутами и дата атрибутами

--- script/script.js
+++ script/script.js
@@ -275,5 +275,31 @@ window.addEventListener("DOMContentLoaded", () => {
 		startSlide(1800);
 	};
 	slider();
+	//запрет ввода всего кроме цифр в калькуляторе
+	const inputArray = [document.querySelector('.calc-square'), document.querySelector('.calc-count'), document.querySelector('.calc-day')];
+	inputArray.forEach((item) => {
+		item.addEventListener('input', () => {
+			item.value = item.value.replace(/\D/, '');
+		});
+	});
+	//По наведению мышкой меняется фотография
+	const mouseOver = e => {
+		const target = e.target;
+
+		if (target.matches('.command__photo')) {
+			target.dataset.image = target.src;
+			target.src = target.dataset.img;
+
+		}
+	};
+	const mouseOut = e => {
+		const target = e.target;
+
+		if (target.matches('.command__photo')) {
+			target.src = target.dataset.image;
+		}
+	};
+	document.getElementById('command').addEventListener('mouseover', mouseOver);
+	document.getElementById('command').addEventListener('mouseout', mouseOut);
 });
 //15:12
