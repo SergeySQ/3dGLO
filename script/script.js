21571c56718e94d96415fa9f266fba0016c8e91c
sergey <mketsvil2@gmail.com>
2020-09-10 04:27:49 +0600

    feat:Animate input

--- script/script.js
+++ script/script.js
@@ -301,5 +301,54 @@ window.addEventListener("DOMContentLoaded", () => {
 	};
 	document.getElementById('command').addEventListener('mouseover', mouseOver);
 	document.getElementById('command').addEventListener('mouseout', mouseOut);
+	//Калькулятор
+	const calc = (price = 100) => {
+		const numAnimation = (totalValue, from, to, duration) => {
+			let elem = totalValue,
+				start = new Date().getTime();
+			let timerId = setTimeout(function tick() {
+				const now = (new Date().getTime()) - start;
+				let progress = now / duration,
+					result = Math.floor((to - from) * progress + from);
+				elem.textContent = progress < 1 ? result : to;
+				if (progress < 1) timerId = setTimeout(tick, 10);
+			}, 10);
+		};
+		const calcBlock = document.querySelector('.calc-block'),
+			calcType = document.querySelector(".calc-type"),
+			calcSquare = document.querySelector('.calc-square'),
+			calcCount = document.querySelector('.calc-count'),
+			calcDay = document.querySelector('.calc-day'),
+			totalValue = document.getElementById('total');
+		const countSum = () => {
+			let total = 0,
+				countValue = 1,
+				dayValue = 1;
+			const typeValue = calcType.options[calcType.selectedIndex].value,
+				squareValue = parseInt(calcSquare.value);
+			if (calcCount.value > 1) {
+				countValue += (calcCount.value - 1) / 10;
+			}
+			if (calcDay.value && calcDay.value <= 5) {
+				dayValue *= 2;
+			} else if (calcDay.value && calcDay.value <= 10) {
+				dayValue *= 1.5;
+			}
+			if (typeValue && squareValue) {
+				total = price * typeValue * squareValue * countValue * dayValue;
+			}
+
+
+			numAnimation(totalValue, +totalValue.textContent, total, 500);
+		};
+
+		calcBlock.addEventListener("change", (e) => {
+			const target = e.target;
+			if (target.matches('select') || target.matches('input')) {
+				countSum();
+			}
+		});
+	};
+	calc(100);
 });
 //15:12
