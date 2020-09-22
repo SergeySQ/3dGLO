fba1c4f3def85df2cd83bd473cca3a5e88a170b9
sergey <mketsvil2@gmail.com>
2020-09-22 19:47:22 +0600

    fix:toggle

--- src/modules/togglePopUp.js
+++ src/modules/togglePopUp.js
@@ -1,57 +1,47 @@
-'use strict';
-//popup
+"use strict";
 const togglePopUp = () => {
 	const popUp = document.querySelector(".popup"),
 		popUpBtn = document.querySelectorAll(".popup-btn"),
-		popUpContent = document.querySelector(".popup-content");
-	let widthWindow = document.documentElement.clientWidth;
-	//отслеживает размер нашего окна
-	window.addEventListener("resize", () => {
-		widthWindow = document.documentElement.clientWidth;
+		popupContent = document.querySelector(".popup-content");
 
-	});
-	//Анимашка
-	let count = 0,
-		rideInterval;
-	const showPopUp = () => {
-		rideInterval = requestAnimationFrame(showPopUp);
-		count += 10;
-		if (count < (widthWindow - 1250) / 2) {
-			popUpContent.style.top = count + "px";
-			popUpContent.style.left = 800 + "px";
+	let internal;
+	let count = -10;
+	const animateModal = () => {
+		const width = document.documentElement.clientWidth;
+
+		popUp.style.display = "block";
+		internal = requestAnimationFrame(animateModal);
+		count += 2;
+
+		if (width <= 768) {
+			popUp.style.display = "block";
+			cancelAnimationFrame(internal);
+		} else if (count < 20) {
+			popupContent.style.top = count + "%";
 		} else {
-			cancelAnimationFrame(rideInterval);
+			cancelAnimationFrame(internal);
 		}
 	};
-	//Запуск нашей анимации и показывает наше модальное окно
+
 	popUpBtn.forEach((elem) => {
-		elem.addEventListener("click", () => {
-			if (widthWindow > 768) {
-				rideInterval = requestAnimationFrame(showPopUp);
-				count = 0;
-
-			} else {
-				popUpContent.style.top = (widthWindow - 250) / 2 + "px";
-
-			}
-			popUp.style.display = "block";
-		});
+		elem.addEventListener("click", animateModal);
 	});
 
-	//Закрытие модального окна
-	popUp.addEventListener("click", (e) => {
-		let target = e.target;
+	popUp.addEventListener("click", () => {
+		let target = event.target;
 
 		if (target.classList.contains("popup-close")) {
 			popUp.style.display = "none";
+			count = -10;
 		} else {
 			target = target.closest(".popup-content");
+
 			if (!target) {
 				popUp.style.display = "none";
+				count = -10;
 			}
 		}
 	});
-
-
 };
+
 export default togglePopUp;
